ac618751a078bf84bd87bd89089bab85945019ca
Mariel Soledad Quintana <msq_lok90@hotmail.com>
2016-06-23 17:04:02 -0300

    Desarrollo inicial del ABM Maquinaria.

---
+++ modulos/maquinaria/php/ver_maquinaria_busqueda.php
@@ -0,0 +1,172 @@
+<?php
+require_once "../../../php/check.php";
+include "../../../lib/link_mysql.php";
+require_once "../../../lib/template.inc";
+include "../../../php/funciones_comunes.php";
+session_start();
+
+$t = new Template('../templates/');
+//Archivos comunes
+$t->set_file(array(
+	"ver"				=> "ver_maquinaria_busqueda.html",
+	"un"				=> "un_maquinaria.html",
+	"una_opcion"		=> "una_opcion.html",
+));
+ 
+$offset=isset($_POST['offset']) ? intval($_POST['offset']) : '';
+$id_tablamodulo=isset($_POST['id_tablamodulo']) ? intval($_POST['id_tablamodulo']) : NULL;
+$titulo="Listado de maquinaria";
+$t->set_var("titulo",$titulo);
+
+//
+//Otras Funciones
+//$t->set_var("funcion_excel","modulos/libros/php/exportar_excel.php?tipo=xls");
+//$t->set_var("funcion_doc","modulos/libros/php/exportar_excel.php?tipo=doc");
+//$t->set_var("funcion_pdf","modulos/libros/php/exportar_excel.php");
+//
+
+$url="'modulos/maquinaria/php/ver_maquinaria.php'";
+$id="'listado_maquinaria'";
+$vars="'es_buscar=si&id_tablamodulo=$id_tablamodulo&";	
+$vars.="tabla22_nombre='+ver_busqueda_maquinaria.tabla22_nombre.value";	
+$t->set_var("funcion_busqueda","cargar_post($url,$id,$vars)");
+
+if (isset($_POST['offset'])) {
+	$offset=$_POST["offset"];
+} else {
+	$offset = "";
+} 
+
+// New Paginador
+$totalporpag=10;
+if(!$offset){ 
+	$off=0;$offset=1;
+}
+else{
+    $off=($offset-1);
+}
+$ini=$off*$totalporpag;
+// End New	
+$sql="select * from tabla_22_maquinaria 
+		order by tabla22_marca ASC  
+		Limit $totalporpag OFFSET $ini ";
+$rs = $pdo->query($sql);//
+$num_rows = $rs->rowCount();
+if ($num_rows>0)
+{
+	while ($row = $rs->fetch())
+	{
+		$id_tabla22=$row["id_tabla22"];
+		$t->set_var("tabla22_imagen",$row["tabla22_imagen"]);
+		$t->set_var("tabla22_nombre",$row["tabla22_nombre"]);
+		$t->set_var("tabla22_descripcion",htmlentities($row["tabla22_descripcion"],ENT_QUOTES));
+		$t->set_var("tabla22_marca",htmlentities($row["tabla22_marca"],ENT_QUOTES));
+		$t->set_var("tabla22_modelo",htmlentities($row["tabla22_modelo"],ENT_QUOTES));
+		$t->set_var("tabla22_fecha_compra",$row["tabla22_fecha_compra"]);
+		$t->set_var("tabla22_costo_compra",$row["tabla22_costo_compra"]);
+		$t->set_var("tabla22_matricula",$row["tabla22_matricula"]);
+        $t->set_var("tabla22_empresa_seguro",$row["tabla22_empresa_seguro"]);
+        $t->set_var("tabla22_rto",$row["tabla22_rto"]);
+        $t->set_var("tabla22_funcion",$row["tabla22_funcion"]);
+		
+		$url="'modulos/maquinaria/php/ver_maquinaria_abm.php'";
+		$id="'tabs-$id_tablamodulo'";
+		$vars="'offset=$offset&id_tablamodulo=$id_tablamodulo&id_tabla22=$id_tabla22'";
+		$t->set_var("funcion_editar","cargar_post($url,$id,$vars)");	
+		
+		$url="'modulos/maquinaria/php/abm_maquinaria_interfaz.php'";
+		$vars="'nombre_funcion=borrar_maquinaria&";
+		$vars.="id_tabla22=$id_tabla22'";
+		$url_exito="'modulos/maquinaria/php/ver_maquinaria_busqueda.php'";
+		$id="'tabs-$id_tablamodulo'";
+		$vars_exito="'offset=$offset&id_tablamodulo=$id_tablamodulo'";
+		$msg="'¿Esta seguro que quiere eliminar el Registro?'";
+		$t->set_var("funcion_borrar","eliminar_mostrar($url,$vars,$url_exito,$id,$vars_exito,$msg);");
+		
+		$t->parse("LISTADO","un",true);
+	}
+}	
+else
+{
+	$t->set_var("LISTADO","<tr align='center' class='alt'><td colspan='10'>No se encuentran Registros Cargados. </td></tr>");
+	
+}	
+	$qrT="select * from tabla_22_maquinaria";
+	$rs = $pdo->query($qrT);//
+	$totalregistros = $rs->rowCount();
+	$t->set_var("cantidad",$totalregistros);
+	$totalpaginas=$totalregistros/$totalporpag;
+	$test=split("\.",$totalpaginas);
+	$pag=''; 
+	if(isset($test[1]))
+	{
+		$totalpaginas=$test[0]+1;
+	}
+	// << Anterior
+	if($offset>1)
+	{
+		$pag.="<td><a href=\"javascript:cargar_post('modulos/maquinaria/php/ver_maquinaria.php','listado_maquinaria','offset=$off&id_tablamodulo=$id_tablamodulo');\"><< Anterior</a> | </td>";
+	}
+	else
+	{
+		$pag.="<td></td>";
+	}
+						 
+	// Numeros 
+	if($totalpaginas>15)
+	{
+		$faltan=($totalpaginas-$off);
+		if($faltan<15)
+		{
+			$ter=$totalpaginas;
+			$com=$off -(15-($totalpaginas-$off));
+		}
+		elseif($off<8)
+		{
+			$ter=15;
+			$com=1;
+		}
+		else
+		{
+			$ter=$off+7;
+			$com=$off-7;
+		}
+	}
+	else
+	{
+		$com=1;
+		$ter=$totalpaginas;
+	}
+		  
+	$pag.="<td align='center'>";
+	for($i=$com;$i<=$ter;$i++)
+	{
+		if($i==$offset)
+		{
+			$pag.="<font color=#000000><b>$i</b></font>&nbsp;";
+		}
+		else
+		{
+			$pag.="<a href=\"javascript:cargar_post('modulos/maquinaria/php/ver_maquinaria.php','listado_maquinaria','offset=$i&id_tablamodulo=$id_tablamodulo');\">$i</a>&nbsp;";
+		}				 	 
+	}
+	$pag.="</td>";
+					// Siguiente >>
+	if($offset<$totalpaginas)
+	{
+		$ofs=$offset+1;
+		$pag.="<td> | <a href=\"javascript:cargar_post('modulos/maquinaria/php/ver_maquinaria.php','listado_maquinaria','offset=$ofs&id_tablamodulo=$id_tablamodulo');\">Siguiente >></a></td>";
+	}else{
+		$pag.="<td></td>";
+	}
+	$t->set_var("paginas","<table align=center><tr>".$pag."</tr></table>");
+		//End Paginador
+	
+	$url="'modulos/maquinaria/php/ver_maquinaria_abm.php'";
+	$id="'tabs-$id_tablamodulo'";
+	$vars="'offset=$offset&id_tablamodulo=$id_tablamodulo'";
+	$t->set_var("funcion_agregar","cargar_post($url,$id,$vars);");
+	$t->set_var("icono_agregar","icon-plus.gif");	
+
+$t->pparse("OUT", "ver");
+?>
